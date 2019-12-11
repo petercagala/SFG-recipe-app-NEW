9f6825b4df738e44482e6f4a5a74f1ea42beae53
Peter Čagala <petercagala@gmail.com>
2019-12-11 07:19:41 +0100

    [0-1] Initial commit

---
+++ recipe-app-core/src/main/java/sk/cagalpte/udemy/sfg/recipeapp/SfgRecipeApplicationNewApplication.java
@@ -0,0 +1,13 @@
+package sk.cagalpte.udemy.sfg.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeApplicationNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRecipeApplicationNewApplication.class, args);
+    }
+
+}
---
+++ recipe-app-core/src/test/java/sk/cagalpte/udemy/sfg/recipeapp/SfgRecipeApplicationNewApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.cagalpte.udemy.sfg.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeApplicationNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
