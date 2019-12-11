5e1c6ebea05b1a738eb972d4e731f1f4f64aaff4
Peter Čagala <petercagala@gmail.com>
2019-12-11 07:43:39 +0100

    [1-ľ] Enum Difficulty, converter

---
+++ recipe-app-core/recipe-app-model/src/main/java/sk/cagalpte/udemy/sfg/recipeapp/mappers/enum_converters/DifficultyConverter.java
@@ -0,0 +1,26 @@
+package sk.cagalpte.udemy.sfg.recipeapp.mappers.enum_converters;
+
+import sk.cagalpte.udemy.sfg.recipeapp.enums.Difficulty;
+
+import javax.persistence.AttributeConverter;
+
+public class DifficultyConverter implements AttributeConverter<Difficulty, Character> {
+
+    @Override
+    public Character convertToDatabaseColumn(Difficulty difficulty) {
+        if(difficulty == null) {
+            return null;
+        } else {
+            return difficulty.getCode();
+        }
+    }
+
+    @Override
+    public Difficulty convertToEntityAttribute(Character character) {
+        if(character == null) {
+            return null;
+        } else {
+            return Difficulty.getEnumFormCode(character);
+        }
+    }
+}
